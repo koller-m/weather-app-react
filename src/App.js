cc958386ac58db5851c399884fc3ed1b7acca65c
Mike Koller <mkoller@eml.cc>
2022-10-31 10:51:01 -0600

    Outline structure of app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 function App() {
+  // Left off 12:00
   return (
     <div className="App">
-      <h2>React is working</h2>
+      <div className="container">
+        <div className="top">
+          <p>Denver</p>
+        </div>
+        <div className="temp">
+          <h1>60 F</h1>
+        </div>
+        <div className="description">
+          <p>Sunny</p>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65 F</p>
+          </div>
+          <div className="humidity">
+            <p>10%</p>
+          </div>
+          <div className="wind">
+            <p>10 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
